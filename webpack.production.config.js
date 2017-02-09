abf366075f5c2de131ba3d67c9c97fa2bb038a80
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-02-09 01:26:16 +0000

    fixed

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -22,6 +22,12 @@ var config = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+        BABEL_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress:{
         warnings: true
