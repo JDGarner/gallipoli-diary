e99c22a8b81b9715b34e352efee0929086fb8a61
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-02-14 00:18:01 +0000

    Add arrow animation

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,8 @@ class Home extends React.Component {
     return (
       <div className="home">
         <h1>A Gallipoli Diary</h1>
-        <h2>Private Frederick Richard Goddard</h2>
+        <h2>Private Frederick Goddard</h2>
+        <div className="arrow-down"></div>
         <div className="main-image"></div>
       </div>
     );
